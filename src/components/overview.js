7eec15a1bcd03d5cb0060dd17cbb421fddf4c9d0
Jehan Hadjisaid <57952278+jehan023@users.noreply.github.com>
2023-02-18 00:18:53 +0800

    Jehan (#13)
    * test commit jehan
    
    * test commit
    
    * test
    
    * test2
    
    * test commit 12345
    
    * test
    
    * test8
    
    * test23
    
    * test commit
    
    * helo world
    
    * Add existing file
    
    * Navbar and Component pages
    
    * overview - profile (template)

--- src/components/overview.js
+++ src/components/overview.js
@@ -1,10 +1,117 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.scss';
 
+import { GiSolarPower } from "react-icons/gi";
+import { FaCarBattery } from "react-icons/fa";
+import { ImPower } from "react-icons/im";
+import { TbBulb } from "react-icons/tb";
+
+
 function Overview() {
+  const devices = [
+    { list: 'SL1', value: 'SL1', label: 'Streetlight 1' },
+    { list: 'SL2', value: 'SL2', label: 'Streetlight 2' },
+    { list: 'SL3', value: 'SL3', label: 'Streetlight 3' },
+  ];
+
+  const [selectedValue, setSelectedValue] = useState(devices[0].value);
+
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value);
+  };
   return (
     <div className='overview-container'>
-        <h1>OVERVIEW</h1>
+      <div className='d-flex align-items-center my-0'>
+        <h3 className='my-0'>System Overview</h3>
+      </div>
+
+      <div className='d-flex justify-content-between'>
+        {/** Left side of the screen section*/}
+        <div className='sl-overview py-3'>
+          <div className='d-flex gap-5 mb-3'>
+            <div className='sl-overview-card d-flex align-items-center'>
+              <GiSolarPower className='overview-icon w-100' />
+              <div className='w-100'>
+                <h3>90.6 Wh</h3>
+                <p>Current Yield</p>
+                <h3>0.5 kWh</h3>
+                <p>Highest Yield</p></div>
+            </div>
+            <div className='sl-overview-card d-flex align-items-center'>
+              <ImPower className='overview-icon w-50' />
+              <div className='w-100'>
+                <h3>0.38 kWh</h3>
+                <p>Current Consumption</p>
+                <h3>1.43 kWh</h3>
+                <p>Highest Consumption</p></div>
+            </div>
+          </div>
+
+          <div className='d-flex gap-5 mb-3'>
+            <div className='sl-overview-card d-flex align-items-center'>
+              <FaCarBattery className='overview-icon w-100' />
+              <div className='w-100'>
+                <h2>24 Ah</h2>
+                <p>Battery Capacity</p>
+              </div>
+            </div>
+            <div className='sl-overview-card d-flex align-items-center'>
+              <TbBulb className='overview-icon w-100' />
+              <div className='w-100'>
+                <h2>100W</h2>
+                <p>LED Lamp</p>
+              </div>
+            </div>
+          </div>
+
+          {/** Details for temp and time section*/}
+          <div className='temp-time d-flex justify-content-between align-items-center p-3'>
+            <div className='current-temp tt-item'>
+              <h1>26°C</h1>
+              <h5>Cloudy</h5>
+              <p>Temperature</p>
+            </div>
+            <div className='sunrise-time tt-item'>
+              <h1>6:25 AM</h1>
+              <h5>(GMT+8)</h5>
+              <p>Sunrise</p>
+            </div>
+            <div className='sunset-time tt-item'>
+              <h1>5:53 PM</h1>
+              <h5>(GMT+8)</h5>
+              <p>Sunset</p>
+            </div>
+          </div>
+        </div>
+
+        {/** Right side of the screen section*/}
+
+        <div className='sl-devices p-3'>
+          <div>
+            <h2 className='mb-3'>Devices</h2>
+            <div className='devices-list'>
+              {devices.map((option) => (
+                <div className='d-flex align-items-center gap-2 mb-1'>
+                  <div className='sl-status'> - </div>
+                  <p>{option.label} ({option.list})</p>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div>
+            <h2 className='my-3'>Energy Balance</h2>
+            <div className='energy-balance-chart'>
+              chart
+            </div>
+          </div>
+          <div>
+            <h2 className='my-3'>Location</h2>
+            <p>Narra St. and  Patola St., Brgy. BF International
+              Village, Las Piñas City | 1740</p>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
--- src/components/profile.js
+++ src/components/profile.js
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Profile() {
+function Profile(props) {
+  const streetlight = props.selectedSL;
+  /*const options = [
+    {
+      list: 'SL1', value: 'SL1', label: 'Streetlight 1', location: 'Narra St., Brgy BF International Village, Las Piñas City | 1740',
+      battCapacity: '20Ah', pvPanel: '65Watts', lamp: '100Watts', status: 'Active'
+    },
+    {
+      list: 'SL2', value: 'SL2', label: 'Streetlight 2', location: 'Narra St., Brgy BF International Village, Las Piñas City | 1740',
+      battCapacity: '30Ah', pvPanel: '65Watts', lamp: '120Watts', status: 'Inactive'
+    },
+    {
+      list: 'SL3', value: 'SL3', label: 'Streetlight 3', location: 'Patola St., Brgy BF International Village, Las Piñas City | 1740',
+      battCapacity: '36Ah', pvPanel: '80Watts', lamp: '150Watts', status: 'Active'
+    },
+  ];
+
+  const [selectedValue, setSelectedValue] = useState(options[0].value);
+
+  const getObjectByValue = (value) => {
+    return options.find((obj) => obj.value === value);
+  };
+
+  const streetlight = getObjectByValue(selectedValue);
+
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value);
+  };*/
   return (
     <div className='profile-container'>
-        <h1>PROFILE</h1>
+      <div className='d-flex align-items-center my-0'>
+        <h3 className='my-0'>System Profile</h3>
+      </div>
+
+      <div className='d-flex align-items-center justify-content-center mt-4'>
+        <div className='profile-card p-4'>
+          <p><b>Name:</b> {streetlight.label} ({streetlight.value})</p>
+          <p><b>Location:</b> {streetlight.location}</p>
+          <p><b>Status:</b> {streetlight.status}</p>
+          <p><b>Transceiver:</b>LoRa SX1278 433MHz</p>
+          <p><b>Gateway:</b> NodeMCU ESP8266 WiFi</p>
+          <p><b>Installation Date:</b>TBA</p>
+          <p><b>Last Maintenance:</b>TBA</p>
+          <div className='line-break mb-3'></div>
+          <p><b>PV Panel:</b> {streetlight.pvPanel}</p>
+          <p><b>System Battery Capacity:</b> {streetlight.battCapacity}</p>
+          <p><b>LED Lamp:</b> {streetlight.lamp}</p>
+
+        </div>
+      </div>
     </div>
   )
 }
