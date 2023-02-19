fbcfbd11430fd5ca0dafa247a391efc1a9e9afe7
Jehan Hadjisaid <57952278+jehan023@users.noreply.github.com>
2023-02-19 19:01:48 +0800

    Jehan (#15)
    * test commit jehan
    
    * test commit
    
    * test
    
    * test2
    
    * test commit 12345
    
    * test
    
    * test8
    
    * test23
    
    * test commit
    
    * helo world
    
    * Add existing file
    
    * Navbar and Component pages
    
    * overview - profile (template)
    
    * status-analysis front-end
    
    * Status and Analysis

--- src/components/analysis.js
+++ src/components/analysis.js
@@ -1,22 +1,105 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { FaCalendarAlt } from 'react-icons/fa';
 
 function Analysis() {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [showDatePicker, setShowDatePicker] = useState(false);
+  const minDate = new Date(2023, 1, 1);
+  const [viewMode, setViewMode] = useState('day');
+  const [dateFormat, setDateFormat] = useState({ month: 'long', day: 'numeric', year: 'numeric' });
+
+  const toggleDatePicker = () => {
+    setShowDatePicker(!showDatePicker);
+  };
+  const handleChange = (date) => {
+    setSelectedDate(date);
+    setShowDatePicker(false);
+  };
+  const CustomInput = ({ value, onClick }) => (
+    <FaCalendarAlt className="date-picker-icon" onClick={onClick} />
+  );
+  const handleViewModeChange = (mode) => {
+    setViewMode(mode);
+    if (mode === 'month') {
+      setDateFormat({ month: 'long', year: 'numeric' });
+    } else if ( mode === 'year') {
+      setDateFormat({ year: 'numeric' });
+    } else {
+      setDateFormat({ month: 'long', day: 'numeric', year: 'numeric' });
+    }
+  };
+
+  const options = dateFormat;
+  const formattedDate = selectedDate.toLocaleDateString('en-US', options);
+
+  console.log(formattedDate); // Output: February 19, 2023
+
   return (
     <div className='analysis-container'>
       <div className='d-flex align-items-center my-0'>
         <h3 className='my-0'>System Analysis</h3>
       </div>
-      <div className='d-flex align-items-center mb-3'>
-        <div className='analysis-date-picker w-50'>Date Picker</div>
+      <div className='d-flex align-items-center'>
+        <div className='analysis-date-picker w-50 py-1'>
+          <div className='date-picker-container d-flex justify-content-start align-items-center gap-2'>
+            <CustomInput onClick={toggleDatePicker} />
+            <h6 className='my-0'>{formattedDate}</h6>
+          </div>
+        </div>     {/** DATE PICKER */}
         <div className='analysis-date-type w-100 d-flex justify-content-end gap-2'>
-          <button className='btn date-type-btn'>Day</button>
-          <button className='btn date-type-btn'>Month</button>
-          <button className='btn date-type-btn'>Year</button>
-          <button className='btn date-type-btn'>Total</button>
+          <button className={viewMode === 'day' ? 'btn date-type-btn-active btn date-type-btn' :'btn date-type-btn'} onClick={() => handleViewModeChange('day')}>Day</button>
+          <button className={viewMode === 'month' ? 'btn date-type-btn-active btn date-type-btn' :'btn date-type-btn'} onClick={() => handleViewModeChange('month')}>Month</button>
+          <button className={viewMode === 'year' ? 'btn date-type-btn-active btn date-type-btn' :'btn date-type-btn'} onClick={() => handleViewModeChange('year')}>Year</button>
         </div>
       </div>
-      <div className='analysis-section'>
-        analysis charts
+
+      {showDatePicker && (
+        <div className="calendar-container">
+          <DatePicker
+            selected={selectedDate}
+            onChange={handleChange}
+            showMonthYearPicker={viewMode === 'month'}
+            showYearPicker={viewMode === 'year'}
+            customInput={<></>}
+            popperPlacement="top-start"
+            inline
+            minDate={minDate}
+            maxDate={new Date()}
+          />
+        </div>
+      )}
+
+      <div className='analysis-section mt-3'>
+        {/******* ENERGY CHART ***************************************************/}
+        <div className='energy-chart-section'>
+          <div className='energy-chart'>
+            Energy Chart
+          </div>
+          <div className='energy-chart-label d-flex justify-content-between'>
+            <p>System Generate: </p>
+            <p>Consumption: </p>
+          </div>
+        </div>
+        {/******* CHARGING CHART **************************************************/}
+        <div className='charging-chart-section'>
+          <div className='charging-chart'>
+            Energy Chart
+          </div>
+          <div className='charging-chart-label d-flex justify-content-between'>
+            <p>Charging Time</p>
+            <p>Discharging Time</p>
+          </div>
+        </div>
+        {/******* ENERGY ANALYSIS CHART COMBINED **********************************/}
+        <div className='energy-analysis-section mb-4'>
+          Energy Analysis Chart
+        </div>
+        {/******* BATTERY LEVEL ANALYSIS CHART ************************************/}
+        <div className='battery-analysis-section'>
+          Battery Level Chart
+        </div>
       </div>
     </div>
   )
