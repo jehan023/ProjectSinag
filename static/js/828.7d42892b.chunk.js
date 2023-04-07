2ad0e0d5a37eec4bfc1fdd361e38b68167739fb5
Jehan Hadjisaid <57952278+jehan023@users.noreply.github.com>
2023-04-07 20:38:32 +0800

    Updates

---
+++ static/js/828.7d42892b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksinag_project=self.webpackChunksinag_project||[]).push([[828],{828:function(t,n,e){e.d(n,{w$t:function(){return a}});var r=e(9983);function a(t){return(0,r.w_)({tag:"svg",attr:{version:"1.1",viewBox:"0 0 16 16"},child:[{tag:"path",attr:{d:"M6 0l-6 8h6l-4 8 14-10h-8l6-6z"}}]})(t)}}}]);
+//# sourceMappingURL=828.7d42892b.chunk.js.map
