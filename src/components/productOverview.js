a55f3255c51f1cc38073997a5663eff96c68611b
miakadv <58924234+miakadv@users.noreply.github.com>
2023-04-13 16:07:16 +0800

    Home Page and Product Overview (#28)
    * testing
    
    * dashboard ayos ayos at konting home page
    
    * ios ios ng homepage
    
    * Home Page learn more button (open user manual)
    
    * Home Page content responsiveness
    
    * Footer Links (kulang pa)
    
    * Home Page project-desc fix
    
    * responsiveness fix and logo sliders
    
    * home page revisions + product overview (learn more)
    
    * feature card in home page
    
    * feature card home page revisions
    
    * product overview page
    
    * product overview page
    
    * testing commit after merge(may binago akong konti here
    
    * test commit
    
    * test commit
    
    * home page and product overview revision
    
    * Home Page and Product Overview Revisions
    
    * home page responsiveness (20%)
    
    ---------
    
    Co-authored-by: Jehan Hadjisaid <57952278+jehan023@users.noreply.github.com>

--- src/components/productOverview.js
+++ src/components/productOverview.js
@@ -3,6 +3,12 @@ import '../App.scss';
 import hpi1 from '../images/hp-image-1.gif';
 import hpi2 from '../images/hp-image-2.gif';
 import hpi3 from '../images/hp-image-3.gif';
+import goalimg from '../images/goalimg.png';
+import objimg from '../images/objimg.png';
+import ciimg1 from '../images/ciimg.png';
+import ciimg2 from '../images/ciimg2.png';
+import ciimg3 from '../images/ciimg3.png';
+
 
 function productOverview() {
     
@@ -10,66 +16,90 @@ function productOverview() {
     return (
         <div className='productOverview-container'>
             <div className='header'>
-                <p className='header-desc'>Welcome! We are excited to share with you our project which aims to provide sustainable and efficient illumination for streets while also reducing energy costs and carbon footprint. In this page, you will find detailed information about our project, including technical specifications of our system. Additionally, we will also explain the social and environmental impact of our project, discussing the community benefits and economic contributions that our system can provide. We hope that this web page will give you a better understanding of our project and how it can make a positive impact on your community.</p>
+                <p className='header-desc'>Overview</p>
+                {/* <p className='header-desc2'>Illuminating your community, one street light at a time.</p> */}
             </div>
 
             <div className='content-container'>
-                <p className='content-title'>Goals and Objective</p>
                 <div className='content-desc-container'>
+                    <p className='content-desc'> We are excited to share with you our project which aims to provide sustainable and efficient illumination for streets while also reducing energy costs and carbon footprint. In this page, you will find detailed information about our project, including technical specifications of our system. Additionally, we will also explain the social and environmental impact of our project, discussing the community benefits and economic contributions that our system can provide. We hope that this web page will give you a better understanding of our project and how it can make a positive impact on your community.</p>
+                    <p className='content-title'>Goals and Objectives</p>
                     <p className='content-desc'>At our core, our project is driven by a mission to bring sustainable illumination to communities around the world while reducing maintenance costs. Our goals and objectives are centered around achieving this mission by creating a system for early anomaly detection that is efficient, effective, and user-friendly.</p>
-                    <p className='content-desc-title'>Our Goals</p>
-                    <ul className='content-desc-title-desc'>
-                        <li>Develop a centralized monitoring system that detects anomalies in real-time</li>
-                        <li>Reduce maintenance costs and prolong the lifespan of street lights</li>
-                        <li>Promote sustainable and eco-friendly lighting solutions</li>
-                        <li>Foster community development through the provision of better lighting and increased safety</li>
-                    </ul>
-                    <p className='content-desc-title'>Our Objectives</p>
-                    <ul>
-                        <li>Implement machine learning algorithms for early anomaly detection in the system</li>
-                        <li>Utilize LoRa wireless communication protocol for efficient and secure communication between the lights and the central monitoring system</li>
-                        <li>Utilize renewable energy sources, such as solar power, to reduce energy costs and environmental impact</li>
-                        <li>Provide a user-friendly interface for easy system management and maintenance</li>
-                    </ul>
+                    <div className='GO-container'>
+                        <div className='GO-card'>
+                            <img src={goalimg} alt='goal-img' className='go-img' />
+                            <p className='content-desc-title'>Our Goals</p>
+                            <ul className='content-desc-title-desc'>
+                                <li>Develop a centralized monitoring system that detects anomalies in real-time.</li>
+                                <li>Reduce maintenance costs and prolong the lifespan of street lights.</li>
+                                <li>Promote sustainable and eco-friendly lighting solutions.</li>
+                                <li>Foster community development through the provision of better lighting and increased safety.</li>
+                            </ul>
+                        </div>
+                        <div className='GO-card'>
+                        <img src={objimg} alt='obj-img' className='go-img' />
+                            <p className='content-desc-title'>Our Objectives</p>
+                            <ul>
+                                <li>Implement machine learning algorithms for early anomaly detection in the system.</li>
+                                <li>Utilize LoRa wireless communication protocol for efficient and secure communication.</li>
+                                <li>Utilize renewable energy sources, such as solar power, to reduce energy costs and environmental impact.</li>
+                                <li>Provide a user-friendly interface for easy system management and maintenance.</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
                 
-                <div className='content-desc-container'>
+                <div className='OP-container'>
                     <p className='content-title'>Overview of the Project</p>
                     <p className='content-desc'>Our Lora-based Centralized Monitoring System for Solar Street Lights is an innovative solution that addresses the growing demand for sustainable and efficient street lighting. With our system, we provide real-time monitoring and control of solar street lights, reducing maintenance costs and extending the lifespan of the lights.</p>
                     <p className='content-desc'>Our system utilizes the LoRa wireless communication protocol, which enables efficient and secure communication between the solar street lights and the central monitoring system. This allows for real-time monitoring of the lights, as well as remote control of the lighting system.</p>
                     <p className='content-desc'>One of the key features of our system is the implementation of machine learning algorithms for early anomaly detection. This helps to detect issues with the lighting system before they become major problems, reducing the need for expensive maintenance and repair work.</p>
                     <p className='content-desc'>Our system is designed to be user-friendly and easy to manage. The centralized monitoring system provides a simple interface for system management and maintenance, allowing users to easily monitor the status of each light and make adjustments as needed.</p>
-                    <p className='content-desc'>Overall, our Lora-based Centralized Monitoring System for Solar Street Lights is an innovative solution that promotes sustainable and eco-friendly lighting solutions while reducing maintenance costs and promoting community development. We are excited to share our project with you and look forward to contributing to a brighter and greener future.</p>
+                    <p className='content-desc'>Overall, our Lora-based Centralized Monitoring System for Solar Street Lights is an innovative solution that promotes sustainable and eco-friendly lighting solutions while reducing maintenance costs and promoting community development. We are excited to share our project with you and look forward to contributing to a brighter and greener future.</p>  
                 </div>
 
+                
                 <div className='content-desc-container'>
-                    <p className='content-title'>Techinical Specifications</p>
+                    <p className='content-title'>Technical Specifications</p>
                     <p className='content-desc'>Desc</p>
                 </div>
 
-                <p className='content-title'>Community Impact</p>
+                
                 <div className='content-desc-container'>
+                    <p className='content-title'>Community Impact</p>
                     <p className='content-desc'>Our Lora-based Centralized Monitoring System for Solar Street Lights is designed to make a positive impact on the communities where it is implemented. By providing better lighting and increased safety on the streets, the project can improve the quality of life for residents in the area. Here are some of the ways that our project can have a community impact:</p>
-                    <p className='content-desc-title'>Better Lighting and Increased Safety</p>
-                    <ul>
-                        <li>Improved visibility for drivers and pedestrians</li>
-                        <li>Reduced risk of accidents and crime</li>
-                        <li>Increased feelings of security for residents</li>
-                    </ul>
-                    <p className='content-desc-title'>Sustainable and Greener Environment</p>
-                    <ul>
-                        <li>Reduced energy consumption and carbon footprint</li>
-                        <li>Support for sustainable development and global climate goals</li>
-                        <li>Increased awareness of environmental issues</li>
-                    </ul>
-                    <p className='content-desc-title'>Local Economic Development</p>
-                    <ul>
-                        <li>Reduced energy costs for municipalities</li>
-                        <li>Local job opportunities in installation and maintenance</li>
-                        <li>More resources available for other community projects</li>
-                    </ul>
+                    
+                    <div className='GO-container'>
+                        <div className='GO-card'>
+                            <img src={ciimg1} alt='ci-img' className='go-img' />
+                            <p className='content-desc-title'>Better Lighting and Increased Safety</p>
+                            <ul>
+                                <li>Improved visibility for drivers and pedestrians</li>
+                                <li>Reduced risk of accidents and crime around the area</li>
+                                <li>Increased sense of security for the residents</li>
+                            </ul>
+                        </div>
+                        <div className='GO-card'>
+                            <img src={ciimg2} alt='ci-img' className='go-img' />
+                            <p className='content-desc-title'>Sustainable and Greener Environment</p>
+                            <ul>
+                                <li>Reduced energy consumption and carbon footprint</li>
+                                <li>Support for sustainable development and global climate goals</li>
+                                <li>Increased awareness of environmental issues</li>
+                            </ul>
+                        </div>
+                        <div className='GO-card'>
+                            <img src={ciimg3} alt='ci-img' className='go-img' />
+                            <p className='content-desc-title'>Local Economic Development</p>
+                            <ul>
+                                <li>Reduced energy costs for municipalities</li>
+                                <li>Local job opportunities in installation and maintenance</li>
+                                <li>More resources available for other community projects</li>
+                            </ul>
+                        </div>
+                    </div>
                     <p className='content-desc'>We believe that our project can create a more vibrant and livable community by bringing sustainable illumination for a brighter and greener future.</p>
-
+                        
                 </div>
 
             </div>
