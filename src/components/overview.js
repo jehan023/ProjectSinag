d488d28add8cba7c9b8d10fa05098247adceb0ba
Jehan Hadjisaid <57952278+jehan023@users.noreply.github.com>
2023-05-17 19:44:44 +0800

    edit overview (#49)
    * test commit jehan
    
    * test commit
    
    * test
    
    * test2
    
    * test commit 12345
    
    * test
    
    * test8
    
    * test23
    
    * test commit
    
    * helo world
    
    * Add existing file
    
    * Navbar and Component pages
    
    * overview - profile (template)
    
    * status-analysis front-end
    
    * Status and Analysis
    
    * gsheet, loadData
    
    * API Call fix
    
    * Charts
    
    * Edit Status Backend
    
    * setup gh-pages
    
    * gh-page setup
    
    * gh home
    
    * Computations and Interpretation of Data
    
    * Analysis Chart Data Computation
    
    * Adjustments and Computations
    
    * edit status
    
    * Overview and Analysis fix
    
    * edit battLevelChart
    
    * chart edit
    
    * edit charge column in data format
    
    * Anomaly Report with PDF
    
    * fix Analysis section
    
    * setup cname
    
    * Overview calculation adjustment
    
    * avg.ON less 1min
    
    * edit

--- src/components/overview.js
+++ src/components/overview.js
@@ -202,7 +202,7 @@ function Overview(props, { setHumidityValue }) {
             <div className='sl-overview-card'>
               <div className='card-content'>
                 <FaCarBattery className='sl-icon' />
-                <h3>{data.battCapacity}</h3>
+                <h3>{data.battCapacity.split(' ')[1]}</h3>
                 <p>Battery Capacity</p>
               </div>
             </div>
